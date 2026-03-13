e861bfb507ceb1f451b86b2cf354cc46864ccd48
Sushant Sharma <sushant20.sharma00@gmail.com>
2026-03-13 08:12:17 +0530

    add fonts

---
+++ src/fonts/index.ts
@@ -0,0 +1,16 @@
+import { Inconsolata, Jersey_25, Jockey_One } from "next/font/google";
+
+export const inconsolata = Inconsolata({
+  subsets: ["latin"],
+  weight: ["200", "300", "400", "500", "600"],
+});
+
+export const jersey25 = Jersey_25({
+  subsets: ["latin"],
+  weight: ["400"],
+});
+
+export const jockeyOne = Jockey_One({
+  subsets: ["latin"],
+  weight: ["400"],
+});
