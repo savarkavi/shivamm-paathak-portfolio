5f3e61c480a594d35bfbc43e7be288fae1df4c88
Sushant Sharma <sushant20.sharma00@gmail.com>
2026-03-18 06:48:07 +0530

    remove header component from Hero and move to layout

--- src/components/sections/hero/index.tsx
+++ src/components/sections/hero/index.tsx
@@ -1,11 +1,9 @@
 import HeroGallery from "./HeroGallery";
 import HomeFooter from "./HomeFooter";
-import HomeHeader from "./HomeHeader";
 
 const Hero = () => {
   return (
     <div className="relative h-screen w-full overflow-hidden">
-      <HomeHeader />
       <HeroGallery />
       <HomeFooter />
     </div>
