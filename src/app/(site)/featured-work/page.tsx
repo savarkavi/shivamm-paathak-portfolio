9067e4f4768ce686da81c64f6f8ddb8423ad1fb2
Sushant Sharma <sushant20.sharma00@gmail.com>
2026-03-27 10:18:10 +0530

    refactor: remove unused mouse follower functionality and clean up imports in Page component

--- src/app/(site)/featured-work/page.tsx
+++ src/app/(site)/featured-work/page.tsx
@@ -1,45 +1,13 @@
-"use client";
-
 import FeaturedFooter from "@/components/sections/featured-work/FeaturedFooter";
-import FeaturedGallery from "@/components/sections/featured-work/FeaturedGallery";
-import { useGSAP } from "@gsap/react";
-import gsap from "gsap";
-import { useRef } from "react";
-
-gsap.registerPlugin(useGSAP);
+import FeaturedGallerySlider from "@/components/sections/featured-work/FeaturedGallerySlider";
+import FeaturedThumbnails from "@/components/sections/featured-work/FeaturedThumbnails";
 
 const Page = () => {
-  const mouseFollowerRef = useRef<HTMLDivElement>(null);
-
-  useGSAP(() => {
-    const mouseFollower = mouseFollowerRef.current;
-    if (!mouseFollower) return;
-
-    const xTo = gsap.quickTo(mouseFollower, "x", {
-      duration: 0.6,
-      ease: "power3",
-    });
-    const yTo = gsap.quickTo(mouseFollower, "y", {
-      duration: 0.6,
-      ease: "power3",
-    });
-
-    window.addEventListener("mousemove", (e) => {
-      xTo(e.clientX);
-      yTo(e.clientY);
-    });
-  });
-
   return (
     <div className="relative flex h-screen items-center justify-center overflow-hidden bg-black text-white">
-      <div
-        id="mouse-follower"
-        ref={mouseFollowerRef}
-        className="pointer-events-none absolute top-0 left-0 z-99 rounded-sm bg-white px-2 py-1 text-sm font-bold text-black uppercase"
-      >
-        Hold and Drag
-      </div>
-      <FeaturedGallery />
+      {/* <FeaturedGallery /> */}
+      <FeaturedThumbnails />
+      <FeaturedGallerySlider />
       <FeaturedFooter />
     </div>
   );
