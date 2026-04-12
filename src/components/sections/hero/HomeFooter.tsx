593072ba3a9c9a5215b8fc656fbe649373b1cf48
Sushant Sharma <sushant20.sharma00@gmail.com>
2026-04-12 06:34:19 +0530

    remove blur from home footer text

--- src/components/sections/hero/HomeFooter.tsx
+++ src/components/sections/hero/HomeFooter.tsx
@@ -4,11 +4,11 @@ const HomeFooter = () => {
   return (
     <div className="fixed bottom-0 left-0 z-10 flex w-full flex-col items-center justify-between gap-4 px-4 py-2 text-base leading-4 text-white uppercase before:absolute before:inset-0 before:-z-10 before:mask-[linear-gradient(to_top,black_90%,transparent)] before:backdrop-blur-lg md:text-lg lg:flex-row lg:px-12">
       <h1
-        className={`${anton.className} text-glow self-end text-5xl tracking-wider text-gray-100 blur-[1px] md:text-[80px]`}
+        className={`${anton.className} text-glow self-end text-5xl tracking-wider text-gray-100 md:text-[80px]`}
       >
         Shivamm Paathak
       </h1>
-      <div className="blur-[1px]">
+      <div>
         <p className="select-none">
           Shivamm Paathak is a Fashion & Fine-Art Photographer,
         </p>
