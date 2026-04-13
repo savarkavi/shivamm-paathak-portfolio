a471e890094db2c1d95663c84f58503d607835b2
Sushant Sharma <sushant20.sharma00@gmail.com>
2026-04-13 06:06:55 +0530

    update styling of home footer

--- src/components/sections/hero/HomeFooter.tsx
+++ src/components/sections/hero/HomeFooter.tsx
@@ -2,18 +2,16 @@ import { anton } from "@/fonts";
 
 const HomeFooter = () => {
   return (
-    <div className="fixed bottom-0 left-0 z-10 flex w-full flex-col items-center justify-between gap-4 px-4 py-2 text-base leading-4 text-white uppercase before:absolute before:inset-0 before:-z-10 before:mask-[linear-gradient(to_top,black_90%,transparent)] before:backdrop-blur-lg md:text-lg lg:flex-row lg:px-12">
+    <div className="fixed bottom-0 left-0 z-10 flex w-full flex-col items-center justify-between gap-2 px-4 py-2 text-base leading-4 text-white uppercase before:absolute before:inset-0 before:-z-10 before:mask-[linear-gradient(to_top,black_90%,transparent)] before:backdrop-blur-lg md:text-lg lg:flex-row lg:px-12">
       <h1
-        className={`${anton.className} text-glow self-end text-5xl tracking-wider text-gray-100 md:text-[80px]`}
+        className={`${anton.className} text-glow text-center text-4xl tracking-wider text-gray-100 md:text-[80px] xl:self-end`}
       >
         Shivamm Paathak
       </h1>
-      <div>
+      <div className="text-center text-sm">
         <p className="select-none">
-          Shivamm Paathak is a Fashion & Fine-Art Photographer,
-        </p>
-        <p className="select-none">
-          Exploring the intersection of mythology and modern India.
+          Shivamm Paathak is a Fashion & Fine-Art Photographer, exploring the
+          intersection of mythology and modern India.
         </p>
       </div>
     </div>
