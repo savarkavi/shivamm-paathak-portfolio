8cc0bed50c2fe09d9e7c499b0553675f69475c78
Sushant Sharma <sushant20.sharma00@gmail.com>
2026-04-13 06:04:59 +0530

    remove 3d ash orb scene from root layout and add it to Hero section

--- src/components/sections/hero/index.tsx
+++ src/components/sections/hero/index.tsx
@@ -1,9 +1,11 @@
+import Scene from "@/components/scene/Scene";
 import HeroGallery from "./HeroGallery";
 import HomeFooter from "./HomeFooter";
 
 const Hero = () => {
   return (
     <div className="relative h-screen w-full overflow-hidden">
+      <Scene />
       <HeroGallery />
       <HomeFooter />
     </div>
