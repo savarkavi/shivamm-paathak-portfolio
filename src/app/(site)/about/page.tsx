0a85d5771eeeb4cd34c63455567832d19a32d6ce
Sushant Sharma <sushant20.sharma00@gmail.com>
2026-05-13 11:19:16 +0530

    add grainy effect to about page

--- src/app/(site)/about/page.tsx
+++ src/app/(site)/about/page.tsx
@@ -3,6 +3,15 @@ import AboutSection from "@/components/sections/about";
 const Page = () => {
   return (
     <div>
+      <div
+        className="fixed top-0 left-0 z-99 h-screen w-screen"
+        style={{
+          backgroundImage: "url('/grainy-effect.webp')",
+          mixBlendMode: "hard-light",
+          opacity: 0.09,
+          pointerEvents: "none",
+        }}
+      ></div>
       <AboutSection />
     </div>
   );
