e61639312cd959972a9d3b0239d5a114eafb2c5b
Sushant Sharma <sushant20.sharma00@gmail.com>
2026-05-19 10:34:21 +0530

    fix: update image hostname in next.config and add typegen configuration in sanity.cli

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "picsum.photos",
+        hostname: "cdn.sanity.io",
       },
     ],
   },
