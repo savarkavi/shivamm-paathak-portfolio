72a5fdc5e3b59ed0c8b1272716f5fd53e79c25a4
Sushant Sharma <sushant20.sharma00@gmail.com>
2026-05-23 08:46:57 +0530

    reduce category name size in category footer

--- src/components/sections/category/CategoryFooter.tsx
+++ src/components/sections/category/CategoryFooter.tsx
@@ -8,7 +8,7 @@ const CategoryFooter = ({ categoryName }: CategoryFooterProps) => {
   return (
     <div className="fixed bottom-0 left-0 z-10 flex w-full flex-col items-center justify-between gap-2 px-4 py-2 text-base leading-4 text-white uppercase before:absolute before:inset-0 before:-z-10 before:mask-[linear-gradient(to_top,black_90%,transparent)] before:backdrop-blur-lg md:text-lg lg:flex-row lg:px-12">
       <h1
-        className={`${chonburi.className} text-glow scale-y-110 text-center text-3xl text-gray-100 md:text-7xl xl:self-end`}
+        className={`${chonburi.className} text-glow scale-y-110 text-center text-3xl text-gray-100 md:text-6xl xl:self-end`}
       >
         {categoryName}
       </h1>
